da19530dd8d1debd9a2f39aa1a3cb341ab4d1dea
SAHIL BAIDYA <96975851+sahilbaidya22@users.noreply.github.com>
2022-06-18 16:18:22 +0530

    first

---
+++ includes/header.php
@@ -0,0 +1,27 @@
+ <nav class="navbar fixed-top navbar-expand-lg navbar-dark bg-dark fixed-top">
+      <div class="container">
+        <a class="navbar-brand" href="https://fontmeme.com/futuristic-fonts/"><img src="https://fontmeme.com/permalink/220213/d838c1f33a842f1243ab9741fd492520.png" alt="futuristic-fonts" border="0" height="40"></a>
+        <button class="navbar-toggler navbar-toggler-right" type="button" data-toggle="collapse" data-target="#navbarResponsive" aria-controls="navbarResponsive" aria-expanded="false" aria-label="Toggle navigation">
+          <span class="navbar-toggler-icon"></span>
+        </button>
+        <div class="collapse navbar-collapse" id="navbarResponsive">
+          <ul class="navbar-nav ml-auto">
+            <li class="nav-item">
+              <a class="nav-link" href="index.php">News</a>
+            </li>
+                 <li class="nav-item">
+              <a class="nav-link" href="about-us.php">About Us</a>
+            </li>
+             <li class="nav-item">
+              <a class="nav-link" href="contact-us.php">Contact us</a>
+            </li>
+            </li>
+             <li class="nav-item">
+              <a class="nav-link" href="admin">Admin Login</a>
+            </li>
+  
+  
+          </ul>
+        </div>
+      </div>
+    </nav>
